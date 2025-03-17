bc870354d8505a0263108b22ed3d8d4cd4e6a2f2
CapsNecessary <konieckac@gmail.com>
2025-03-17 12:19:35 +0100

    fixed api

--- api/api.php
+++ api/api.php
@@ -28,32 +28,31 @@
 				}
 				break;
 			case 'POST':
-				$uwiw		= $in[ 'uwiw' ]		 ?? '""';		$kat	 = $in[ 'kat' ]		?? '""';
-				$sala		= $in[ 'sala' ]		 ?? '""';		$lpWSali = $in[ 'lpWSali' ]	?? '""';
-				$model		= $in[ 'model' ]	 ?? '""';		$wyglad	 = $in[ 'wyglad' ]	?? '""';
-				$processor	= $in[ 'processor' ] ?? '""';		$ram	 = $in[ 'ram' ]		?? '""';
-				$plyta		= $in[ 'plyta' ]	 ?? '""';		$dysk	 = $in[ 'dysk' ]	?? '""';
-				$przekatna	= $in[ 'przekatna' ] ?? '""';		$mac	 = $in[ 'mac' ]		?? '""';
-				$licencje	= $in[ 'licencje' ]	 ?? '""';		$inne	 = $in[ 'inne' ]	?? '""';
+				$uwiw		= $in[ 'uwiw' ]		 ?? "''";		$kat	 = $in[ 'kat' ]		?? "''";
+				$sala		= $in[ 'sala' ]		 ?? "''";		$lpWSali = $in[ 'lpWSali' ]	?? "''";
+				$model		= $in[ 'model' ]	 ?? "''";		$wyglad	 = $in[ 'wyglad' ]	?? "''";
+				$processor	= $in[ 'processor' ] ?? "''";		$ram	 = $in[ 'ram' ]		?? "''";
+				$plyta		= $in[ 'plyta' ]	 ?? "''";		$dysk	 = $in[ 'dysk' ]	?? "''";
+				$przekatna	= $in[ 'przekatna' ] ?? "''";		$mac	 = $in[ 'mac' ]		?? "''";
+				$licencje	= $in[ 'licencje' ]	 ?? "''";		$inne	 = $in[ 'inne' ]	?? "''";
 				$q = $c -> prepare( "INSERT INTO `urzadzenia`( `uwiw`, `kategoria`, `sala`, `lpwsali`, `model`, `wyglad`, `procesor`, `ram`, `plyta`, `dysk`, `przekatna`, `mac`, `licencje`, `inne`)
 					VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )" );
-				$q -> bind_param( 'ssssssssssssssss', $uwiw, $kat, $sala, $lpWSali, $model, $wyglad, $processor, $ram, $plyta, $dysk, $przekatna, $mac, $licencje, $inne );
+				$q -> bind_param( 'ssssssssssssss', $uwiw, $kat, $sala, $lpWSali, $model, $wyglad, $processor, $ram, $plyta, $dysk, $przekatna, $mac, $licencje, $inne );
 				$q -> execute();
 				break;
 			case 'PUT':
 				$id = $in[ 'id' ];
 				if( isSet( $id ) ){
-					$uwiw		= $in[ 'uwiw' ]		 ?? '""';		$kat	 = $in[ 'kat' ]		?? '""';
-					$sala		= $in[ 'sala' ]		 ?? '""';		$lpWSali = $in[ 'lpWSali' ]	?? '""';
-					$model		= $in[ 'model' ]	 ?? '""';		$wyglad	 = $in[ 'wyglad' ]	?? '""';
-					$processor	= $in[ 'processor' ] ?? '""';		$ram	 = $in[ 'ram' ]		?? '""';
-					$plyta		= $in[ 'plyta' ]	 ?? '""';		$dysk	 = $in[ 'dysk' ]	?? '""';
-					$przekatna	= $in[ 'przekatna' ] ?? '""';		$mac	 = $in[ 'mac' ]		?? '""';
-					$licencje	= $in[ 'licencje' ]	 ?? '""';		$inne	 = $in[ 'inne' ]	?? '""';
-					// print( json_encode( "{`id`:$id, `uwiw`:$uwiw, `kat`:$kat, `sala`:$sala, `lpWSali`:$lpWSali, `model`:$model, `wyglad`:$wyglad, `processor`:$processor, `ram`:$ram, `plyta`:$plyta, `dysk`:$dysk, `przekatna`:$przekatna, `mac`:$mac, `licencje`:$licencje, `inne`:$inne }" ) );
-					$q = $c -> prepare( "UPDATE `urzadzenia` SET `uwiw` = ?,`kategoria` = ?,`sala` = ?,`lpwsali` = ?,`model` = ?,`wyglad` = ?,`procesor` = ?,`ram` = ?,`plyta` = ?
+					$uwiw		= $in[ 'uwiw' ]		 ?? "''";		$kat	 = $in[ 'kat' ]		?? "''";
+					$sala		= $in[ 'sala' ]		 ?? "''";		$lpWSali = $in[ 'lpWSali' ]	?? "''";
+					$model		= $in[ 'model' ]	 ?? "''";		$wyglad	 = $in[ 'wyglad' ]	?? "''";
+					$processor	= $in[ 'processor' ] ?? "''";		$ram	 = $in[ 'ram' ]		?? "''";
+					$plyta		= $in[ 'plyta' ]	 ?? "''";		$dysk	 = $in[ 'dysk' ]	?? "''";
+					$przekatna	= $in[ 'przekatna' ] ?? "''";		$mac	 = $in[ 'mac' ]		?? "''";
+					$licencje	= $in[ 'licencje' ]	 ?? "''";		$inne	 = $in[ 'inne' ]	?? "''";
+					$q = $c -> prepare( "UPDATE `urzadzenia` SET `uwiw` = ?,`kategoria` = ?,`sala` = ?,`lpwsali` = ?,`model` = ?,`wyglad` = ?,`procesor` = ?,`ram` = ?,`plyta` = ?,
 						`dysk` = ?, `przekatna` = ?,`mac` = ?,`licencje` = ?,`inne` = ? where id=?" );
-					$q -> bind_param( 'ssssssssssssssssi', $uwiw, $kat, $sala, $lpWSali, $model, $wyglad, $processor, $ram, $plyta, $dysk, $przekatna, $mac, $licencje, $inne, $id );
+					$q -> bind_param( 'ssssssssssssssi', $uwiw, $kat, $sala, $lpWSali, $model, $wyglad, $processor, $ram, $plyta, $dysk, $przekatna, $mac, $licencje, $inne, $id );
 					$q -> execute();
 				}
 				else http_response_code( 418 );
