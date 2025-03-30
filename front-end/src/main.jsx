109a939a3d508e7c5d7d7d410c2fc9c3e08223da
CapsNecessary <konieckac@gmail.com>
2025-03-30 22:24:16 +0200

    fixed dark and light mode importing and commented out colors in Tabela.css

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,7 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import "../../css/dark-and-light-mode.css";
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
