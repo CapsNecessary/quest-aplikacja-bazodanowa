1e0eb18829b9c1533703dcd14b54410849b7f0db
CapsNecessary <konieckac@gmail.com>
2025-03-31 12:16:25 +0200

    mostly finished styling

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import "../../css/dark-and-light-mode.css";
 import './index.css';
 import App from './App.jsx';
-import "../../css/dark-and-light-mode.css";
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
