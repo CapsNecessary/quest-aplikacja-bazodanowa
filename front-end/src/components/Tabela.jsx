d40948ede17ececfd59a55adde192989515cce85
CapsNecessary <konieckac@gmail.com>
2025-04-11 14:44:56 +0200

    fixed the warning: Each child in a list should have a unique “key” prop. for Tabela.jsx

--- front-end/src/components/Tabela.jsx
+++ front-end/src/components/Tabela.jsx
@@ -21,31 +21,33 @@ export const Tabela = () => {
 								selectedCategory={selectedCategory} 
 								setSelectedCategory={setSelectedCategory} 
 						/> </div>
-				<div className="uwiw">Ewidencja</div>
-				<div className="sala">Sala</div>
-				<div className="kategoria">Kategoria</div>
-				<div className="model">Model</div>
-				<div className="obudowa">Obudowa</div>
-				<div className="data-zakupu">Data zakupu</div>
-				<div className="status">Status</div>
-				<div className="operacje operacje4x">Operacje</div>
+				<div className="wiersz">
+					<div className="uwiw">Ewidencja</div>
+					<div className="sala">Sala</div>
+					<div className="kategoria">Kategoria</div>
+					<div className="model">Model</div>
+					<div className="obudowa">Obudowa</div>
+					<div className="data-zakupu">Data zakupu</div>
+					<div className="status">Status</div>
+					<div className="operacje operacje4x">Operacje</div>
+				</div>
 				{/*devices = filterDevices(devices,selectedCategory)*/}
-				{devices.map(device => (
-					<>
-						<div className="uwiw">{device.uwiw}</div>
-						<div className="sala">{device.sala}</div>
-						<div className="kategoria">{device.kategoria}</div>
-						<div className="model">Model: {device.model}<br />{device.przekatna && `Przekątna: ${device.przekatna}`}<br /></div>
-						<div className="obudowa">{device.obudowa}</div>
-						<div className="data-zakupu">{device.data_zakupu}</div>
-						<div className="status">{device.status_}</div>
-						<div className="operacje"><a href={`#operacja=edit&id=${device.id}`} className="button">Edytuj</a></div>
-						<div className="operacje"><a href={`#operacja=przen&id=${device.id}`} className="button">Przenieś</a></div>
-						<div className="operacje"><a href={`#operacja=del&id=${device.id}`} className="button">Kasacja</a></div>
-						<div className="operacje"><a href={`#operacja=hist&id=${device.id}`} className="button">Historia</a></div>
-					</>
+				{ devices.map( device => (
+					<div className="wiersz" key={ device.id }>
+						<div className="uwiw">{ device.uwiw }</div>
+						<div className="sala">{ device.sala }</div>
+						<div className="kategoria">{ device.kategoria }</div>
+						<div className="model">Model: { device.model }<br />{ device.przekatna && `Przekątna: ${ device.przekatna }` }<br /></div>
+						<div className="obudowa">{ device.obudowa }</div>
+						<div className="data-zakupu">{ device.data_zakupu }</div>
+						<div className="status">{ device.status_ }</div>
+						<div className="operacje"><a href={ `#operacja=edit&id=${ device.id }` } className="button">Edytuj</a></div>
+						<div className="operacje"><a href={ `#operacja=przen&id=${ device.id }` } className="button">Przenieś</a></div>
+						<div className="operacje"><a href={ `#operacja=del&id=${ device.id }` } className="button">Kasacja</a></div>
+						<div className="operacje"><a href={ `#operacja=hist&id=${ device.id }` } className="button">Historia</a></div>
+					</div>
 				))}
-				<div className="szeroki-div-grida">Liczba pozycji: {initialDevices.length}</div>
+				<div className="szeroki-div-grida">Liczba pozycji: { initialDevices.length}</div>
 			</div>
 		</div>
 	);
