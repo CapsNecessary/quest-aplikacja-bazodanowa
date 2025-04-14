3f3f0c1b5657333f0653c1687fcabeb1ea7fa7e8
CapsNecessary <konieckac@gmail.com>
2025-04-14 11:49:00 +0200

    working on making filters work pt.0

--- front-end/src/components/PrzyciskiKategorii.jsx
+++ front-end/src/components/PrzyciskiKategorii.jsx
@@ -1,6 +1,7 @@
 import { kategorie } from "./Consts";
 
 export const PrzyciskiKategorii = ({ selectedCategory, setSelectedCategory }) => {
+<<<<<<< Updated upstream
 		return (
 			<div>
 				<h3>Filtruj kategorię</h3>
@@ -17,3 +18,20 @@ export const PrzyciskiKategorii = ({ selectedCategory, setSelectedCategory }) =>
 		);
 	};
 	
+=======
+	return (
+		<div>
+			<h3>Filtruj kategorię</h3>
+			{kategorie.map( ( category, index ) => (
+				<button 
+					key={index} 
+					className="filtr-button"
+					onClick={ () => setSelectedCategory(category) }
+					>
+						{ category }
+				</button>
+			))}
+		</div>
+	);
+};
+>>>>>>> Stashed changes
