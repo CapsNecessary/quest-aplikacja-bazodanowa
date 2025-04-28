89ae3237aeb353f8f906c3a1f35c2e3bb7602fb4
CapsNecessary <konieckac@gmail.com>
2025-04-28 10:57:33 +0200

    showing current category and something else

--- front-end/src/components/Filtry.jsx
+++ front-end/src/components/Filtry.jsx
@@ -1,51 +1,50 @@
 import { useState } from "react";
 import { kategorie, uwiwOptions } from "./Consts";
 
-export const filterDevices = (devices, selectedCategory) => {
-		return devices.filter(device => 
-			(selectedCategory === "Wszystkie" || device.kategoria === selectedCategory) 
-		);
-	};
+export const filterDevices = ( devices, selectedCategory ) => {
+	const ret = devices.filter( device => { console.log( device.kategoria ); return selectedCategory === "Wszystkie" || device.kategoria === selectedCategory.toLowerCase() } );
+	console.log( devices, selectedCategory, ret )
+	return ret;
+};
 
 const SelectKategoria = ({ selectedCategory, setSelectedCategory }) => {
-		return (
-			<div>
-				Wybierz kategorię
-				<select className="filtr-select">
-					{kategorie.map((category, index) => (
-						<option 
-								key={index} 
-								value={category}
-								onClick={() => setSelectedCategory(category)}>
-							{category}
-						</option>
-					))}
-				</select>
-			</div>
-		);
-	};
-	const SelectUwiw = () => {
-		return (
-			<div>
-				Ewidencja/UWIW
-				<select name="uwiw" className="filtr-select">
-					{uwiwOptions.map((option, index) => (
-						<option key={index} value={option.value}>
-							{option.label}
-						</option>
-					))}
-				</select>
-			</div>
-		);
-	};
+	return (
+		<div>
+			Wybierz kategorię
+			<select className="filtr-select">
+				{ kategorie.map(( category, index ) => (
+					<option 
+							key={ index } 
+							value={ category }
+							onClick={ () => setSelectedCategory( category ) }>
+						{ category }
+					</option>
+				))}
+			</select>
+		</div>
+	);
+};
 
-
+const SelectUwiw = () => {
+	return (
+		<div>
+			Ewidencja/UWIW
+			<select name="uwiw" className="filtr-select">
+				{uwiwOptions.map((option, index) => (
+					<option key={index} value={option.value}>
+						{option.label}
+					</option>
+				))}
+			</select>
+		</div>
+	);
+};
 
 export const Filtry = ({ selectedCategory, setSelectedCategory }) => {
 		return <div>
 				<SelectKategoria 
-						selectedCategory={selectedCategory} 
-						setSelectedCategory={setSelectedCategory} 
+						selectedCategory={ selectedCategory } 
+						setSelectedCategory={ setSelectedCategory } 
 				/>
 				<SelectUwiw />
 				...
