fb36d6e60be7736e67794b35acc4f6f8fb0dfb92
CapsNecessary <konieckac@gmail.com>
2025-05-12 11:17:03 +0200

    made the connection work

--- front-end/src/components/Tabela.jsx
+++ front-end/src/components/Tabela.jsx
@@ -19,8 +19,26 @@ export const Tabela = () => {
 	const fetchUrzadzenia = async () => {
 		try{
 			const response = await axios.get( API_Get );
-			let res = response.data
-			for (let i = 0; i < res.length; i++) res[ i ] = res[ i ].replace( `["`, '' ).replace( /\"\]$/, '' ).split( '","' );
+			let res = response.data;
+			console.log( res.join() );
+			for (let i = 0; i < res.length; i++){
+				res[ i ][ "id" ]			= res[ i ][ 0 ];
+				res[ i ][ "uwiw" ]			= res[ i ][ 1 ];
+				res[ i ][ "kategoria" ]		= res[ i ][ 2 ];
+				res[ i ][ "sala" ]			= res[ i ][ 3 ];
+				res[ i ][ "lpwsali" ]		= res[ i ][ 4 ];
+				res[ i ][ "model" ]			= res[ i ][ 5 ];
+				res[ i ][ "obudowa" ]		= res[ i ][ 6 ];
+				res[ i ][ "procesor" ]		= res[ i ][ 7 ];
+				res[ i ][ "ram" ]			= res[ i ][ 8 ];
+				res[ i ][ "plyta" ]			= res[ i ][ 9 ];
+				res[ i ][ "dysk" ]			= res[ i ][ 10 ];
+				res[ i ][ "przekatna" ]		= res[ i ][ 11 ];
+				res[ i ][ "mac" ]			= res[ i ][ 12 ];
+				res[ i ][ "licencje" ]		= res[ i ][ 13 ];
+				res[ i ][ "inne" ]			= res[ i ][ 14 ];
+				//`id`, `uwiw`, `kategoria`, `sala`, `lpwsali`, `model`, `wyglad`, `procesor`, `ram`, `plyta`, `dysk`, `przekatna`, `mac`, `licencje`, `inne`
+			}
 			setUrzadzenia( res );
 			console.log( res );
 		} catch( error ){
@@ -58,8 +76,8 @@ export const Tabela = () => {
 						<div className="kategoria">{ device.kategoria }</div>
 						<div className="model">Model: { device.model }<br />{ device.przekatna && `Przekątna: ${ device.przekatna }` }<br /></div>
 						<div className="obudowa">{ device.obudowa }</div>
-						<div className="data-zakupu">{ device.data_zakupu }</div>
-						<div className="status">{ device.status_ }</div>
+						<div className="data-zakupu">{ !!device?.data_zakupu ? device.data_zakupu : "Brak" }</div>
+						<div className="status">{ !!device?.status ? device.status : "Brak" }</div>
 						<div className="operacje"><a href={ `#operacja=edit&id=${ device.id }` } className="button">Edytuj</a></div>
 						<div className="operacje"><a href={ `#operacja=przen&id=${ device.id }` } className="button">Przenieś</a></div>
 						<div className="operacje"><a href={ `#operacja=del&id=${ device.id }` } className="button">Kasacja</a></div>
